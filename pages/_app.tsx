1f76291aad06511c4e3a44843c8ef055f1e02f08
Naomi Schettini <naomischettini@gmail.com>
2022-06-04 03:47:37 +0200

    converted old javascript to typescript. fking nextjs so many issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,117 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
+    padding: 0;
+    margin: 0;
+    font-family: "Avenir";
+    background-color: #E3F2FD;
+    color: #1E2A27;
+    box-sizing: border-box;
+    font-size: 9pt;
+    transform-style: preserve-3d;
+    overflow-x:hidden;
+    overflow-y:auto;
+  }
+  
+  title {
+    background-color: "#ACAD94";
+    font-family: "Avenir";
+    font-size: 30pt;
+    font-weight: 700;
+    text-align: left;
+    padding-left: 400px;
+    padding-right: 44px;
+    padding-top: 60px;
+    position: fixed;
+  }
+  
+  link {
+    font-family: "Avenir";
+    color: #384D48;
+  }
+  
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  
+  ul {
+    background-color: #F4FEC1;
+    list-style-type: none;
     margin: 0;
     padding: 0;
+    left: 0px;
+    right: 0px;
+    top: 0px;
+    position: fixed;
+    overflow: hidden;
+    border-top: 1px solid #A9531B;
+    border-bottom: 1px solid #A9531B;
+    width: 100%;
+  }
+  
+  li {
+    display: inline;
+    float: left;
+  }
+  
+  li a {
+    display:block;
+    color:#A9531B;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-weight: 500;
+    font-family: "Avenir";
+    font-size: 16px;
+  }
+  
+  li a:hover {
+    background-color:#A9531B;
+    color:#F4FEC1;
+  }
+  
+  li:last-child {
+    border-right: none;
+  }
+  blockquote { 
+    background-image: url(http://typespiration.com/wp-content/themes/typespiration/images/quote.png); 
+    background-position: 10px 10%; 
+    background-repeat: no-repeat no-repeat; 
+    padding: 150px 0; 
+  }
+  blockquote p { 
+    color: #F4FEC1; 
+    background: #A9531B; 
+    font-size: 75px; 
+    line-height: 74px; 
+    font-weight: 700; 
+    margin: 0 5px 24px; 
+    float: left; 
+    padding: 10px;
+     margin: 0 5px 24px; 
+     font-family: 'Libre Baskerville', serif;
+   }
+  h2 a {
+     color: #adadad; 
+     font-size: 25px; 
+     text-decoration: none; 
+     float: right; 
+     font-family: 'Libre Baskerville', 
+     serif; line-height: 4; 
+    }
+  h2 a:hover { 
+    color: #A9531B;
+     text-decoration: none;
+     }
+  * {
     box-sizing: border-box;
   }
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: '#E3F2FD',
   },
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,87 @@
 import styled from 'styled-components'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <html>
+    <div className={styles.container}>
+      <Head>
+    <title className={styles.punchline}>a regenerative agtech incubator</title>
+       {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.navContainer}>
+        <ul className={styles.navList}>  
+        <li><a href="">Home</a></li>
+        <li><a href="">Blueprint</a></li>
+        <li><a href="">Monetization</a></li>
+        <li><a href="">Functions and Benefits</a></li>
+        <li><a href="">Tokens</a></li>
+        <li><a href="">Contact</a></li>
+        </ul>
+        </div>
+        
+        <h1 className={styles.title}>
+         A Regenerative AgTech Incubator <a href="https://www.cryptoaltruism.org/blog/daos-and-the-future-of-social-impact">Impact DAO</a>
+        </h1>
+
+        <div className={styles.tagline}>
+        A modern infrastructure model for decentralized community living and food security innovation incubator.
+        </div>
+
+        <div className={styles.subtitle}>
+        -- Maximizing quality of life through ecological care, self development and <b>regenerative technologies</b>. A possible solution to a core problem in the human experience: <i>loneliness and belonging</i>.
+        </div>
+        <body>
+        <div className="parallax-wrapper">
+            <div className="content">
+                 <h1>what we stand for</h1>
+                
+                What does it mean to fit together? Maybe, to have everything in its place. A natural order. An order in which there is no strain of mind, of energy, thought. An order which comes naturally, from a state of freedom. 
+
+Freedom, however, is not simply doing what one wants when one wants. When free to act impulsively or from a state of ones primal instincts, one realizes that some of the choices we make dont always come from a place of individual true choice, or are not in our best common interest.Our primitive and selfish behavior as a species continue to confound a lack of meaning and systemic inequalities with our most crucial and pressing crisis: climate change.- We endlessly seek material/superficial answers to existential questions, perpetuating deception, hypocrisy, and disappointment.. Freedom therefore comes with great responsibility. 
+
+As technology races ahead of our own collective psychological evolution, a positive way forward is to ensure we are redefining our meaning to life. Decentralized cities allow young people to migrate toward rural areas and engage with projects they care about to improve the world and have a stable yet transient (agrotourism) community they can relate to. Many people dont want to be tied to one location anymore. The meaning of ownership and value of material possessions is changing. So is the way we value ourselves. We are leaving the reductionist capitalist model for one where everyone has an opportunity to live up to their potential if given compassion, resources and the exposure needed to blossom.If given compassion, exposure, and most importantly the resources. 
+
+We are not idealists. This is just the next phase of human evolution: a life beyond the me.
+
+To redefine societal values and orient a new approach of modern civilization, we must cultivate an environment which evokes the desire for: 
+
+Cooperative decisions for all humans - require minds which question and value observing large scope of content before drawing conclusions
+Environments that inspire -  inner growth, maturity and self-awareness, not escapism
+Silent retreats and nature - interactions with the natural world to inhibit peace of mind
+Elimination of emphasis on status in society - reinventing/integrating harmoniously w/ AI
+
+It is now clear:
+-	the globalized society humankind has created is unequivocally fragile
+-	society must redefine its values to for a sustainable maximization of living quality
+-	remote work has become a norm
+-	mankinds interaction with the natural world must drastically change and include indigenous wisdom
+-  humans need to exercise their fulfillment in a new way of living that will consist of mostly leisure without following into destructive vices
+             
+            </div>
+        </div>
+        <div className="regular-wrapper">
+            <div className="content">
+                <p><h1> The Blueprint </h1></p>
+            </div>
+        </div>
+        </body>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
+      </footer>
+    </div>
+    </html>
+  )
 }
